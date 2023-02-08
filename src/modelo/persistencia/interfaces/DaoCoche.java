bc9b4c8de41dc1711b46d6bc78b4f57138d2c790
Isra <pirravk@gmail.com>
2023-02-08 12:42:11 +0100

    Ducumentamos las interfaces

--- src/modelo/persistencia/interfaces/DaoCoche.java
+++ src/modelo/persistencia/interfaces/DaoCoche.java
@@ -5,10 +5,43 @@ import java.util.List;
 import modelo.entidad.Coche;
 
 public interface DaoCoche {
+	
+	/**
+	 * Metodo que da de alta un coche en la BBDD. Se generará el ID de manera
+	 * automática.
+	 * @param c el coche a dar de alta
+	 * @return true en caso de que se haya dado de alta. false en caso de error
+	 * con la BBDD.
+	 */
 	public boolean altaCoche(Coche c);
+	/**
+	 * Metodo que da de baja un coche en la BBDD. 
+	 * @param id del coche a dar de baja.
+	 * @return true en caso de que se haya dado de alta. false en caso de error
+	 * con la BBDD.
+	 */
 	public boolean bajaCoche(int id);
+	/**
+	 * Metodo que modifica un coche en la BBDD. La modificación será a partir
+	 * del ID que contenga el coche.
+	 * @param c el coche a modificar
+	 * @return true en caso de que se haya modificado. False en caso de error
+	 * con la BBDD.
+	 */
 	public boolean modificarCoche(Coche c);
+	/**
+	 * Metodo para consultar un coche en la BBDD. 
+	 * @param id del coche a consultar.
+	 * @return el coche consultado o mensaje de error en caso de error
+	 * con la BBDD.
+	 */
 	public Coche consultaCoche(int id);
+	/**
+	 * Metodo para listar todos los coches de la BBDD. 
+	 * @return la lista de coches de la BBDD o mensaje de error en caso de error
+	 * con la BBDD.
+	 */
 	public List<Coche> listarCoches();
 
 }
+
--- src/modelo/persistencia/interfaces/DaoPasajero.java
+++ src/modelo/persistencia/interfaces/DaoPasajero.java
@@ -6,13 +6,59 @@ import modelo.entidad.Pasajero;
 
 public interface DaoPasajero {
 	
+	/**
+	 * Metodo que da de alta un pasajero en la BBDD. Se generará el ID de manera
+	 * automática.
+	 * @param p el pasajero a dar de alta
+	 * @return true en caso de que se haya dado de alta. false en caso de error
+	 * con la BBDD.
+	 */
 	public boolean altaPasajero(Pasajero p);
+	/**
+	 * Metodo que da de baja un pasajero en la BBDD. 
+	 * @param id del pasajero a dar de baja.
+	 * @return true en caso de que se haya dado de alta. false en caso de error
+	 * con la BBDD.
+	 */
 	public boolean bajaPasajero(int id);
+	/**
+	 * Metodo para consultar un pasajero en la BBDD. 
+	 * @param id del pasajero a consultar.
+	 * @return el coche consultado o mensaje de error en caso de error
+	 * con la BBDD.
+	 */
 	public Pasajero consultaPasajero(int id);
+	/**
+	 * Metodo para listar todos los pasajeros de la BBDD. 
+	 * @return la lista de pasajeros de la BBDD o mensaje de error en caso de error
+	 * con la BBDD.
+	 */
 	public List<Pasajero> listarPasajeros();
+	/**
+	 * Metodo que añade un pasajero a un coche en la BBDD. Se generará el ID de manera
+	 * automática.
+	 * @param idPas el id del pasajero a añadir.
+	 * @param idCoche el id del coche en el que se añade el pasajero.
+	 * @return true en caso de que se haya añadido. False en caso de error
+	 * con la BBDD.
+	 */
 	public boolean addPasajeroACoche(int idPas, int idCoche);
+	/**
+	 * Metodo que borra un pasajero de un coche en la BBDD. 
+	 * @param idPas el id del pasajero a eliminar.
+	 * @param idCoche el id del coche del que se borra el pasajero.
+	 * @return true en caso de que se haya borrado. False en caso de error
+	 * con la BBDD.
+	 */
 	public boolean borrarPasajeroDeCoche(int idPas, int idCoche);
+	/**
+	 * Metodo para listar todos los pasajeros de un coche de la BBDD. 
+	 * @param idCoche el id del coche del que mostramos los pasajeros.
+	 * @return la lista de pasajeros del coche o mensaje de error en caso de error
+	 * con la BBDD.
+	 */
 	public List<Pasajero> listarPasajerosCoche(int idCoche);
 	
 
 }
+
